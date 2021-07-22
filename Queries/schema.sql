22d76143ceef40692c973f099b8a180ecb7d7770
corispade <cjspade44@gmail.com>
2021-07-21 20:24:52 -0400

    adding table documentation

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -15,7 +15,7 @@ CREATE TABLE employees (
 	gender VARCHAR NOT NULL,
 	hire_date DATE NOT NULL,
 	PRIMARY KEY (emp_no));
-	
+
 CREATE TABLE dept_manager (
 	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
@@ -33,17 +33,18 @@ CREATE TABLE salaries (
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no));
 
+DROP TABLE titles CASCADE;
 CREATE TABLE titles (
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no));
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
+	PRIMARY KEY (emp_no, title, from_date));
 
-CREATE TABLE dept_employees (
-	dept_no VARCHAR(4) NOT NULL,
+CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -52,3 +53,8 @@ CREATE TABLE dept_employees (
 	
 -- Confirm code by using select statements
 SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM dept_manager;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
