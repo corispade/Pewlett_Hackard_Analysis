66497d67000080ba39a08495287da464a4177d34
corispade <cjspade44@gmail.com>
2021-07-21 21:05:07 -0400

    updating queries and schema notes

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -1,92 +1,45 @@
 schema.sql
 
--- Creating tables for PH-EmployeeDB
-CREATE TABLE departments (
-	dept_no VARCHAR(4) NOT NULL,
-	dept_name VARCHAR(40) NOT NULL,
-	PRIMARY KEY (dept_no),
-	UNIQUE (dept_name));
-	
-CREATE TABLE employees (
-	emp_no INT NOT NULL,
-	birth_date DATE NOT NULL,
-	first_name VARCHAR NOT NULL,
-	last_name VARCHAR NOT NULL,
-	gender VARCHAR NOT NULL,
-	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no));
+-- Notes for QuickDBD
 
-CREATE TABLE dept_manager (
-	dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no));
-	
-CREATE TABLE salaries (
-	emp_no INT NOT NULL,
-	salary INT NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no));
+Departments
+--
+dept_number int pk
+dept_name varchar
 
-DROP TABLE titles CASCADE;
-CREATE TABLE titles (
-	emp_no INT NOT NULL,
-	title VARCHAR NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no) ON DELETE CASCADE,
-	PRIMARY KEY (emp_no, title, from_date));
+Employees
+--
+emp_no int pk
+birth_date date
+first_name varchar
+last_name varchar
+gender varchar
+hire_date date
 
-CREATE TABLE dept_emp (
-	emp_no INT NOT NULL,
-	dept_no VARCHAR(4) NOT NULL,
-	from_date DATE NOT NULL,
-	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-	PRIMARY KEY (emp_no, dept_no));
-	
--- Confirm code by using select statements
-SELECT * FROM departments;
-SELECT * FROM dept_emp;
-SELECT * FROM dept_manager;
-SELECT * FROM employees;
-SELECT * FROM salaries;
-SELECT * FROM titles;
+Managers
+--
+dept_number varchar pk fk - Departments.dept_number
+emp_no int fk - Employees.emp_no
+from_date date
+to_date date
 
--- Find list of employees about to retire
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+Dept_Emp
+--
+emp_no int pk fk >- Employees.emp_no
+dept_no int pk fk - Departments.dept_number
+from_date date
+to_date date
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+Salaries
+--
+emp_no int pk fk - Employees.emp_no
+salary int
+from_date date
+to_date date
 
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
-
-SELECT first_name, last_name
-FROM employees
-WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
-
--- Retirement eligibility
-SELECT first_name, last_name
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Create retirement table
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31') 
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info;
+Titles
+--
+emp_no int pk fk >- Employees.emp_no
+title varchar
+from_date date
+to_date date
